14bd03184a7d5c4202c6216bc89c0e68cd04736b
O.Remilekun <remmylknolaotan@gmail.com>
2022-07-31 19:07:05 +0100

    Add comments to code

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 const container = document.querySelector('.container');
 const button = document.querySelector('button');
 
+// Function to render desired grids
+function renderGrid(squares) {
+    for (let i = 0; i < (squares * squares); i++) {
+        const div = document.createElement('div');
+        
+        // Add box class to each div
+        div.classList.add('box');
+        
+        container.appendChild(div);
+    }
+    
+    // Reset the grid template columns and rows each time
+    container.style.gridTemplateColumns = `repeat(${squares}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${squares}, 1fr)`;
+};
+
+// Color grids when mouse enters it
 const grids = document.querySelectorAll('.box');
 grids.forEach(grid => {
     grid.addEventListener('mouseenter', () => {
@@ -8,19 +25,7 @@ grids.forEach(grid => {
     }) 
 })
 
-// Create a default 16x16 grid of square divs
-let newSquares = 16;
-function renderGrid(squares) {
-    for (let i = 0; i < (squares * squares); i++) {
-        const div = document.createElement('div');
-        div.classList.add('box');
-        container.appendChild(div);
-    }
-    container.style.gridTemplateColumns = `repeat(${squares}, 1fr)`;
-    container.style.gridTemplateRows = `repeat(${squares}, 1fr)`;
-};
-renderGrid(newSquares);
-
+// Clear current grid when new grid is set
 function clearGrid() {
     const boxes = document.querySelectorAll('.box');
     boxes.forEach(box => {
@@ -28,6 +33,12 @@ function clearGrid() {
     })
 }
 
+// Create a default 16x16 grid in container
+let newSquares = 16;
+renderGrid(newSquares);
+
+
+// Reset the grids to the desired dimensions
 button.addEventListener('click', () => {
     newSquares = prompt('Enter number of squares per side for the new grid. (max: 100)', '3');
     clearGrid()
